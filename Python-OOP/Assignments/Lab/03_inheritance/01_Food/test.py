c232a7d3cc29fa634a8a11033659e1c959897d4d
Angelin Georgiev <georgiev.angelin@gmail.com>
2024-07-18 16:45:46 +0300

    Uploaded assignment from the course

---
+++ Python-OOP/Assignments/Lab/03_inheritance/01_Food/test.py
@@ -0,0 +1,6 @@
+from project.food import Food
+from project.fruit import Fruit
+
+f = Fruit("apple", expiration_date="2024-07-10")
+print(f.name)
+print(f.expiration_date)
---
+++ Python-OOP/Assignments/Lab/03_inheritance/04_multilevel_inheritance/test.py
@@ -0,0 +1,8 @@
+from project.sports_car import SportsCar
+from project.car import Car
+
+vehicle = SportsCar()
+
+print(vehicle.race())
+print(vehicle.drive())
+print(vehicle.move())
